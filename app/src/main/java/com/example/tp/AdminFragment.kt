ccaefc5c2ee1426854637809f24466885aea7663
sanjanadate <sanjana.date@cumminscollege.in>
2020-12-31 12:15:47 +0530

    My tpapp

--- app/src/main/java/com/example/tp/AdminFragment.kt
+++ app/src/main/java/com/example/tp/AdminFragment.kt
@@ -4,11 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,43 +16,26 @@ private const val ARG_PARAM2 = "param2"
  * Use the [AdminFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-
 class AdminFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
-
         }
-
     }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view  =  inflater.inflate(R.layout.fragment_admin, container, false)
-        val logbutton: Button = view.findViewById(R.id.btn_register)
-        logbutton.setOnClickListener {
-            val username:TextView = view.findViewById(R.id.email_placeholder_admin)
-            val password:TextView = view.findViewById(R.id.password_placeholder_admin)
-            if(username.text.toString().equals("abc")) {
-                if(password.text.toString().equals("admin")) {
-                    println("Hi")
-                }
-            }
-        }
-        return view
+        return inflater.inflate(R.layout.fragment_admin, container, false)
     }
-
 
     companion object {
         /**
