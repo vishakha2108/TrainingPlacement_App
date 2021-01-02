d8b7f0240bfb916da10bbfff4e8bfb8b3b819abf
sanjanadate <sanjana.date@cumminscollege.in>
2021-01-02 15:17:28 +0530

    fragment_student_view_companies.xml fragment_admin_add_company.xml fragment_admin_delete_company.xml

--- app/src/main/java/com/example/tp/LoginFragment.kt
+++ app/src/main/java/com/example/tp/LoginFragment.kt
@@ -34,7 +34,7 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_view_students, container, false)
+        return inflater.inflate(R.layout.fragment_onboarding, container, false)
     }
 
     companion object {
